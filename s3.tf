d1a7fc62c5fbe5c134a12f4087835b6f2c3f240d
seanih <sean.anih@gmail.com>
2026-04-24 21:34:05 -0400

    removed KMS for simplification

--- s3.tf
+++ s3.tf
@@ -23,8 +23,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "s3_encryption" {
     bucket_key_enabled = true
 
     apply_server_side_encryption_by_default {
-      sse_algorithm     = "aws:kms"
-      kms_master_key_id = aws_kms_key.s3_key.arn
+      sse_algorithm     = "AES256"
     }
   }
 }
